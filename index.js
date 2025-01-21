366b1cd06c61184c0b017db0ce8f3dbd06462412
Crispin Manda <crispinmanda06@gmail.com>
2025-01-21 08:05:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// variables
+
+var x = 11;
+console.log(x);
+
+// constants
+const y = 10;
+console.log(y);
+
+console.log(typeof y);
+
+let username = 'John';
+console.log(username);
+console.log(typeof username);
+
+const result = 10 > 5;
+console.log(result);
+
+console.log(typeof result);
+let cars = ['volvo', 'toyota', 'bMW'];
+
+console.log(typeof(cars));
+console.log (cars[1]);
+console.log (cars[2]);
+console.log (cars);
